84c57f924078a9b6f09b4159dab6f096a1b2677e
buckeyerichsen <buckey.erichsen@gmail.com>
2019-09-13 09:22:42 -0400

    finished

--- src/Main.java
+++ src/Main.java
@@ -3,92 +3,32 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-//       Random r = new Random();
-        String[] rice ={"white rice, ", "brown rice, ", "no rice, "};
-        String[] meat={"chicken meat, ", "steak meat, ", "carnidas meat, ", "chorizo meat, ", "sofritas meat, ", "veggies, "};
-        String[] beans={"pinto beans, ", "black beans, ", "no beans, "};
-        String[] salsa={"mild salsa, ", "medium salsa, ", "hot salsa, ", "no salsa, "};
-        String[] veggies={"lettuce, ", "fajitas, ", "veggies, ", "no veggies, "};
 
-        Random r=new Random();
-
-        System.out.println("Burrito 1: "
-                + rice[r.nextInt(rice.length)]
-                + meat[r.nextInt(meat.length)]
-                + beans[r.nextInt(beans.length)]
-                + salsa[r.nextInt(salsa.length)]
-                + veggies[r.nextInt(veggies.length)]);
-
-        System.out.println("Burrito 2: "
-                + rice[r.nextInt(rice.length)]
-                + meat[r.nextInt(meat.length)]
-                + beans[r.nextInt(beans.length)]
-                + salsa[r.nextInt(salsa.length)]
-                + veggies[r.nextInt(veggies.length)]);
-
-        System.out.println("Burrito 3: "
-                + rice[r.nextInt(rice.length)]
-                + meat[r.nextInt(meat.length)]
-                + beans[r.nextInt(beans.length)]
-                + salsa[r.nextInt(salsa.length)]
-                + veggies[r.nextInt(veggies.length)]);
-
-        System.out.println("Burrito 4: "
-                + rice[r.nextInt(rice.length)]
-                + meat[r.nextInt(meat.length)]
-                + beans[r.nextInt(beans.length)]
-                + salsa[r.nextInt(salsa.length)]
-                + veggies[r.nextInt(veggies.length)]);
-
-        System.out.println("Burrito 5: "
-                + rice[r.nextInt(rice.length)]
-                + meat[r.nextInt(meat.length)]
-                + beans[r.nextInt(beans.length)]
-                + salsa[r.nextInt(salsa.length)]
-                + veggies[r.nextInt(veggies.length)]);
-
-        System.out.println("Burrito 6: "
-                + rice[r.nextInt(rice.length)]
-                + meat[r.nextInt(meat.length)]
-                + beans[r.nextInt(beans.length)]
-                + salsa[r.nextInt(salsa.length)]
-                + veggies[r.nextInt(veggies.length)]);
-
-        System.out.println("Burrito 7: "
-                + rice[r.nextInt(rice.length)]
-                + meat[r.nextInt(meat.length)]
-                + beans[r.nextInt(beans.length)]
-                + salsa[r.nextInt(salsa.length)]
-                + veggies[r.nextInt(veggies.length)]);
-
-        System.out.println("Burrito 8: "
-                + rice[r.nextInt(rice.length)]
-                + meat[r.nextInt(meat.length)]
-                + beans[r.nextInt(beans.length)]
-                + salsa[r.nextInt(salsa.length)]
-                + veggies[r.nextInt(veggies.length)]);
-
-        System.out.println("Burrito 9: "
-                + rice[r.nextInt(rice.length)]
-                + meat[r.nextInt(meat.length)]
-                + beans[r.nextInt(beans.length)]
-                + salsa[r.nextInt(salsa.length)]
-                + veggies[r.nextInt(veggies.length)]);
-
-        System.out.println("Burrito 10: "
-                + rice[r.nextInt(rice.length)]
-                + meat[r.nextInt(meat.length)]
-                + beans[r.nextInt(beans.length)]
-                + salsa[r.nextInt(salsa.length)]
-                + veggies[r.nextInt(veggies.length)]);
+        ArrayList<String> newList = new ArrayList<>();
 
 
+        String[] rice = {"white rice, ", "brown rice, ", "no rice, "};
+        String[] meat = {"chicken meat, ", "steak meat, ", "carnidas meat, ", "chorizo meat, ", "sofritas meat, ", "veggies, "};
+        String[] beans = {"pinto beans, ", "black beans, ", "no beans, "};
+        String[] salsa = {"mild salsa, ", "medium salsa, ", "hot salsa, ", "no salsa, "};
+        String[] veggies = {"lettuce, ", "fajitas, ", "veggies, ", "no veggies, "};
+
+        Random r = new Random();
+
+        for (int i = 1; i < 11; i++) {
+
+            String burrito = ("Burrito " + i + ": "
+                    + rice[r.nextInt(rice.length)]
+                    + meat[r.nextInt(meat.length)]
+                    + beans[r.nextInt(beans.length)]
+                    + salsa[r.nextInt(salsa.length)]
+                    + veggies[r.nextInt(veggies.length)]);
+
+            newList.add(burrito);
+
+            System.out.println(burrito);
 
 
-
-
-
-
-
+        }
     }
 }
